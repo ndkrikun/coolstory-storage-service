bd054ba2504adb6e0f2b677a6e33e1a24ca9f08c
Nikita Krikun <ndkrikun@gmail.com>
2017-11-26 00:17:09 +0300

    Added terminal log for characters

--- app/api/controllers/character-controllers.ts
+++ app/api/controllers/character-controllers.ts
@@ -1,24 +1,29 @@
 import { model } from 'mongoose'
+import { requestMessage } from '../../libs/terminal-messages'
 
 const Character = model('Character')
 
 export const allCharacters = (req, res) => {
+  requestMessage(false, req.method, 'Read all characters')
   Character.find({}, (err, character) => {
     err && res.send(err)
+    requestMessage(true, req.method, 'Returned characters list')
     res.json(character)
   })
 }
 
 export const createCharacter = (req, res) => {
   const newCharacter = new Character(req.body)
-
+  requestMessage(false, req.method, 'Create new character')
   if (Object.keys(req.body).length === 0) {
+    requestMessage(true, req.method, 'Failed to create new character')
     res.json({
       message: 'Failed to save the character. Empty body'
     })
   } else {
     newCharacter.save((err, character) => {
       err && res.send(err)
+      requestMessage(true, req.method, 'New character was created')
       res.json(character)
     })
   }
@@ -26,10 +31,12 @@ export const createCharacter = (req, res) => {
 
 export const readCharacter = (req, res) => {
   const id = req.params.characterId
+  requestMessage(false, req.method, `Read character ${id}`)
   Character.findById(
     id,
     (err, character) => {
       err && res.send(err)
+      requestMessage(true, req.method, `Character was readed ${id}`)
       res.json(character)
     }
   )
@@ -37,12 +44,14 @@ export const readCharacter = (req, res) => {
 
 export const updateCharacter = (req, res) => {
   const id = req.params.characterId
+  requestMessage(false, req.method, `Update character ${id}`)
   Character.findOneAndUpdate(
     { _id: id },
     req.body,
     { new: true }, 
     (err, character) => {
       err && res.send(err)
+      requestMessage(true, req.method, `Character was updated ${id}`)
       res.json(character)
     }
   )
@@ -50,10 +59,12 @@ export const updateCharacter = (req, res) => {
 
 export const removeCharacter = (req, res) => {
   const id = req.params.characterId
+  requestMessage(false, req.method, `Remove character ${id}`)
   Character.remove(
     { _id: id },
     err => {
       err && res.send(err)
+      requestMessage(true, req.method, `Character was removed ${id}`)
       res.json({
         id,
         message: 'Character successfully deleted'
--- app/libs/terminal-messages.ts
+++ app/libs/terminal-messages.ts
@@ -1,8 +1,7 @@
 export const requestMessage = (complete: Boolean, method: String, name: String): void => {
-  const line: String = '----------------------------------------------------------------------------'
+  const line: String = '---------------------------------------------'
   const arrow: String = complete ? '<-----' : '----->'
   
-  !complete && console.log(`\n${line}\n`)
   console.log(`${arrow} ${method} | ${name}`)
-  complete && console.log(`${line}\n`)
+  complete && console.log(`\n${line}\n`)
 }
